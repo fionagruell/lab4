7f0ef5db0411baf1f884a3e8c8fdd28cdf01360d
Fiona Gruell <Fiona@pc10.pool.tp1.uni-duesseldorf.de>
2015-11-17 11:19:28 +0100

    haben wir toll gemacht

--- deriv.cxx
+++ deriv.cxx
@@ -10,10 +10,33 @@ void print(double* p, const int N, const double dx,
    for(int i=0; i<N; i++)
        cout << i*dx + xmin << "\t" << p[i] << endl;
 }
+void calc(double* p, const int N, const double xmin, const double dx){
+  for(int i=0; i<N; i++){
+   double x=xmin+i*dx; 
+  p[i]=exp(-x*x);
+  }  
+}
+void der(double* p, const int N, const double dx){
+  double p0=0;
+  double p1=p[0];
+  double pp=p[1];
+  
+  p[0]=(p[1]-p0)/(2*dx);
+  
+  for(int i=1; i<N; i++){
+    pp=p[i];
+    p[i]=(p[i+1]-p1)/(2*dx);
+    p1=pp;
+    
+    
+  }
+  p[N-1]=(p0-p1)/(2*dx);
+}
+  
 
 
 int main(){
-  const int N = 256;
+  const int N = 100;
   double p[N];
   const double xmax = 15;
   const double xmin = -15;
@@ -21,6 +44,8 @@ int main(){
 
   // call to function which fills array p here
   // call to functio which calculates the derivative
+  calc(p,N,xmin,dx);
+  der(p,N,dx);
   print(p,N,dx,xmin);
 
   return 0;
